22ff6cb6badaa005af3776d07fa390702e9c3964
史宇韬 <yutao.shi@baishancloud.com>
2018-02-02 15:49:06 +0800

    [feat] 1.2.0 版本发布

--- dist/api.js
+++ dist/api.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.BsFetch = undefined;
+exports.Fetch = undefined;
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
@@ -17,46 +17,53 @@ var _http_request = require('./http_request');
 
 var _igroot = require('igroot');
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// URL 完整性校验
+var urlRegxp = /^https?:\/\//;
 
 // error 弹出锁, 避免弹出框过多
 var errorLock = false;
 
-// function warp(method) {
-//   return (data, showSuccessMsg = true) => {
-//     return httpRequest[method](this.url, data, {}, {})
-//       .then(res => res.json())
-//       .then(json => {
-//         if (json.code !== 0) {
-//           // 弹出提示框
-//           if (!errorLock) {
-//             errorLock = true
-//             setTimeout(() => errorLock = false, 2000)
+// 域名缓存，避免每次请求都去匹配域名
+var defaultDomain = void 0;
 
-//             notification.error({
-//               message: `请求失败 code: ${json.code}`,
-//               description: json.msg ? json.msg : json.message || ''
-//             })
-//           }
-//         } else
-//           showSuccessMsg && notification.success({
-//             message: '请求成功',
-//             description: json.msg ? json.msg : ''
-//           })
+function warp(method) {
+  return function (data) {
+    return _http_request.httpRequest.post(this.url, data, this._buildOptions()).then(function (res) {
+      return res.json();
+    }).then(function (json) {
+      if (json.code !== 0) {
+        // 弹出提示框
+        if (!errorLock) {
+          errorLock = true;
+          setTimeout(function () {
+            return errorLock = false;
+          }, 2000);
 
-//         return json
-//       })
+          _igroot.notification.error({
+            message: '\u8BF7\u6C42\u5931\u8D25 code: ' + json.code,
+            description: json.msg ? json.msg : json.message || ''
+          });
+        }
+      } else _igroot.notification.success({
+        message: '请求成功',
+        description: json.msg ? json.msg : ''
+      });
 
-//       .catch(err => {
-//         notification.error({
-//           message: '请求失败',
-//           description: '错误信息请打开控制台查看'
-//         })
+      return json;
+    }).catch(function (err) {
+      _igroot.notification.error({
+        message: '请求失败',
+        description: '错误信息请打开控制台查看'
+      });
 
-//         throw err
-//       })
-//   }
-// }
+      throw err;
+    });
+  };
+}
 
 var RESTful = function () {
   function RESTful(url, configs) {
@@ -65,6 +72,10 @@ var RESTful = function () {
     if (typeof url === 'string') {
       this.url = url;
       this.configs = configs;
+
+      this.post = warp('post').bind(this);
+      this.put = warp('put').bind(this);
+      this.delete = warp('delete').bind(this);
     } else {
       throw new TypeError('API object constructor argument must be the object or string');
     }
@@ -96,108 +107,6 @@ var RESTful = function () {
         });
 
         return res;
-      });
-    }
-  }, {
-    key: 'post',
-    value: function post(data) {
-      return _http_request.httpRequest.post(this.url, data, this._buildOptions()).then(function (res) {
-        return res.json();
-      }).then(function (json) {
-        if (json.code !== 0) {
-          // 弹出提示框
-          if (!errorLock) {
-            errorLock = true;
-            setTimeout(function () {
-              return errorLock = false;
-            }, 2000);
-
-            _igroot.notification.error({
-              message: '\u8BF7\u6C42\u5931\u8D25 code: ' + json.code,
-              description: json.msg ? json.msg : json.message || ''
-            });
-          }
-        } else _igroot.notification.success({
-          message: '请求成功',
-          description: json.msg ? json.msg : ''
-        });
-
-        return json;
-      }).catch(function (err) {
-        _igroot.notification.error({
-          message: '请求失败',
-          description: '错误信息请打开控制台查看'
-        });
-
-        throw err;
-      });
-    }
-  }, {
-    key: 'put',
-    value: function put(data) {
-      return _http_request.httpRequest.put(this.url, data, this._buildOptions()).then(function (res) {
-        return res.json();
-      }).then(function (json) {
-        if (json.code !== 0) {
-          // 弹出提示框
-          if (!errorLock) {
-            errorLock = true;
-            setTimeout(function () {
-              return errorLock = false;
-            }, 2000);
-
-            _igroot.notification.error({
-              message: '\u8BF7\u6C42\u5931\u8D25 code: ' + json.code,
-              description: json.msg ? json.msg : json.message || ''
-            });
-          }
-        } else _igroot.notification.success({
-          message: '请求成功',
-          description: json.msg ? json.msg : ''
-        });
-
-        return json;
-      }).catch(function (err) {
-        _igroot.notification.error({
-          message: '请求失败',
-          description: '错误信息请打开控制台查看'
-        });
-
-        throw err;
-      });
-    }
-  }, {
-    key: 'delete',
-    value: function _delete(data) {
-      return _http_request.httpRequest.delete(this.url, data, this._buildOptions()).then(function (res) {
-        return res.json();
-      }).then(function (json) {
-        if (json.code !== 0) {
-          // 弹出提示框
-          if (!errorLock) {
-            errorLock = true;
-            setTimeout(function () {
-              return errorLock = false;
-            }, 2000);
-
-            _igroot.notification.error({
-              message: '\u8BF7\u6C42\u5931\u8D25 code: ' + json.code,
-              description: json.msg ? json.msg : json.message || ''
-            });
-          }
-        } else _igroot.notification.success({
-          message: '请求成功',
-          description: json.msg ? json.msg : ''
-        });
-
-        return json;
-      }).catch(function (err) {
-        _igroot.notification.error({
-          message: '请求失败',
-          description: '错误信息请打开控制台查看'
-        });
-
-        throw err;
       });
     }
   }]);
@@ -252,7 +161,7 @@ var GraphQL = function GraphQL(url, client) {
   throw new TypeError('GraphQL Client configuration error!');
 };
 
-var BsFetch = exports.BsFetch = function BsFetch(url) {
+var Fetch = exports.Fetch = function Fetch(url) {
   var config = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
   // const token = window.localStorage['JWT_TOKEN'] || window.localStorage['jwtToken'] || ''
@@ -263,17 +172,42 @@ var BsFetch = exports.BsFetch = function BsFetch(url) {
   // }
   // _config = Object.assign({}, _config, config)
 
-  var type = 'restful';
-  if (!!config.type) {
-    type = config.type;
-  } else if (url.indexOf('graphql') >= 0) {
-    type = 'graphql';
+  // 如果不是完整的 url 就给它加上域名
+  if (!urlRegxp.test(url)) {
+    var host = window.location.host;
+
+    // 判断是否有默认域名缓存，如果没有就拿当前的域名遍历匹配，寻找相应的接口地址
+    if (!defaultDomain) {
+      Object.keys(Fetch.domain).forEach(function (domain) {
+        var condition = Fetch.domain[domain];
+
+        if (typeof condition === 'string' && condition === host || condition instanceof RegExp && condition.test(window.location.host)) {
+          defaultDomain = domain;
+        }
+      });
+
+      if (!defaultDomain) throw new Error('Can not match the api domain! Please check your api domain config.');
+    }
+
+    url = defaultDomain + url;
   }
 
-  if (type === 'graphql') {
-    return new GraphQL(url, config);
-  }
-  return new RESTful(url, config);
+  var type = config.type || (url.indexOf('graphql') >= 0 ? 'graphql' : 'restful');
+  var API = type === 'graphql' ? GraphQL : RESTful;
+
+  return new API(url, config);
 };
 
-module.exports = BsFetch;
+Fetch.setDomain = function setDomain(domain) {
+  if (typeof domain === 'string') {
+    if (urlRegxp.test(domain)) return Fetch.domain = _defineProperty({}, domain, /.*/);
+
+    throw new Error('\'domain\' string must be \'http://\' or \'https://\' at the beginning.');
+  }
+
+  if ((typeof domain === 'undefined' ? 'undefined' : _typeof(domain)) === 'object') return Fetch.domain = domain;
+
+  throw new TypeError('\'domain\' type must be a string or object!');
+};
+
+module.exports = Fetch;
